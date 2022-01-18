3f3b55ee06941294740d4dfc671b76656ce150b4
sina alizadeh <sina.alizadeh.120@gmail.com>
2022-01-18 23:21:28 +0330

    hgfhf

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./Counter/counter";
 import AsyncAwait from "./Counter/AsyncAwait";
+import ReactQuery from "./React-query/React-query";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <AsyncAwait />
+      {/* <Counter />
+      <AsyncAwait /> */}
+      <ReactQuery />
     </div>
   );
 }
